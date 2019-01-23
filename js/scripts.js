f7bf7976645119a414728ebc35157b1ffde951f6
Isaac Hall <isaacdehall@gmail.com>
2019-01-22 20:57:04 -0800

    final?

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-var suits = ["spades", "clubs", "hearts", "diamonds"];
-var values= ["2","3","4","5","6","7","8","9","10", "Jack", "Queen", "King", "Ace"];
-var cards = [];
+const suits = ["spades", "clubs", "hearts", "diamonds"];
+const values= ["2","3","4","5","6","7","8","9","10", "Jack", "Queen", "King", "Ace"];
+let cards = [];
 
 suits.forEach(function(suit){
   values.forEach(function(value){
@@ -11,49 +11,81 @@ suits.forEach(function(suit){
 function filterItems(query) {
   return cards.filter(function(el) {
       return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
-  })
+  });
 };
-
-var spades = filterItems('spades');
-console.log(spades);
-var clubs = filterItems('clubs');
-console.log(clubs);
-var hearts = filterItems('hearts');
-var diamonds = filterItems('diamonds');
 var noComma = cards.join('\n');
-alert(noComma)
-
+alert(noComma);
 $(document).ready(function() {
-
-  //   if(filterItems("spades") === true) {
-  //     $("." + suits).text(spades);
-  //   } else if(filterItems("clubs") === true) {
-  //     $("." + suits).text(spades);
-  //   } else {
-  //     console.log("damn");
-  //   }
-
-    $(".spades").text(spades);
-    $(".clubs").text(clubs);
-    $(".hearts").text(hearts);
-    $(".diamonds").text(diamonds);
-
+  suits.forEach(function(sweet){
+    $("." + sweet).text(filterItems(sweet));
+    $("." + sweet).text(filterItems(sweet));
+    $("." + sweet).text(filterItems(sweet));
+    $("." + sweet).text(filterItems(sweet));
+  });
 });
 
-// Map functions
+
+
+
+
+
+
+// var suits = ["spades", "clubs", "hearts", "diamonds"];
 // var values= ["2","3","4","5","6","7","8","9","10", "Jack", "Queen", "King", "Ace"];
+// var cards = [];
 //
-// var spades = values.map(function(value){
-//   return value + " of spades";
-// });
-// var clubs = values.map(function(value){
-//   return value + " of clubs";
-// var hearts = values.map(function(value){
-//   return value + " of hearts";
-// });
-// var diamonds = values.map(function(value){
-//   return value + " of diamonds";
-// });
+// suits.forEach(function(suit){
+//   values.forEach(function(value){
+//     cards.push(" " + value + " of " + suit );
+//   });
 // });
 //
-// alert(hearts + diamonds + clubs + spades);
+// function filterItems(query) {
+//   return cards.filter(function(el) {
+//       return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
+//   })
+// };
+//
+// var spades = filterItems('spades');
+// console.log(spades);
+// var clubs = filterItems('clubs');
+// console.log(clubs);
+// var hearts = filterItems('hearts');
+// var diamonds = filterItems('diamonds');
+// var noComma = cards.join('\n');
+// alert(noComma)
+//
+// $(document).ready(function() {
+//
+//   //   if(filterItems("spades") === true) {
+//   //     $("." + suits).text(spades);
+//   //   } else if(filterItems("clubs") === true) {
+//   //     $("." + suits).text(spades);
+//   //   } else {
+//   //     console.log("damn");
+//   //   }
+//
+//     $(".spades").text(spades);
+//     $(".clubs").text(clubs);
+//     $(".hearts").text(hearts);
+//     $(".diamonds").text(diamonds);
+//
+// });
+//
+// // Map functions
+// // var values= ["2","3","4","5","6","7","8","9","10", "Jack", "Queen", "King", "Ace"];
+// //
+// // var spades = values.map(function(value){
+// //   return value + " of spades";
+// // });
+// // var clubs = values.map(function(value){
+// //   return value + " of clubs";
+// // var hearts = values.map(function(value){
+// //   return value + " of hearts";
+// // });
+// // var diamonds = values.map(function(value){
+// //   return value + " of diamonds";
+// // });
+// // });
+// //
+// // alert(hearts + diamonds + clubs + spades);
